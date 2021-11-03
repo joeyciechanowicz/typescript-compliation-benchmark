f85ec756627483d03d88bd0058a550c3eb3ee02c
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2021-11-03 17:12:50 +0000

    update run

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,11 @@
 
 # brew install hyperfine
 
+mkdir -p dist
+
 node create-templates.js
+
+echo "Generated typescript files"
 
 # safety checks
 ./node_modules/.bin/tsc dist/noTypesReducer.ts
@@ -10,8 +14,12 @@ node create-templates.js
 ./node_modules/.bin/tsc dist/partialTypedState.ts
 ./node_modules/.bin/tsc dist/pickInterfaceState.ts
 
+echo "Typescript files are valid"
+
 hyperfine --min-runs 20 --warmup 3 \
-    './node_modules/.bin/tsc dist/noTypesReducer.ts' \
     './node_modules/.bin/tsc dist/partialState.ts' \
     './node_modules/.bin/tsc dist/partialTypedState.ts' \
-    './node_modules/.bin/tsc dist/pickInterfaceState.ts'
+    './node_modules/.bin/tsc dist/pickInterfaceState.ts' \
+    --export-markdown run.md
+    
+    # './node_modules/.bin/tsc dist/noTypesReducer.ts' \
---
+++ run.md
@@ -0,0 +1,5 @@
+| Command | Mean [s] | Min [s] | Max [s] | Relative |
+|:---|---:|---:|---:|---:|
+| `./node_modules/.bin/tsc dist/partialState.ts` | 5.107 ± 0.185 | 4.782 | 5.547 | 1.39 ± 0.05 |
+| `./node_modules/.bin/tsc dist/partialTypedState.ts` | 4.867 ± 0.157 | 4.706 | 5.232 | 1.32 ± 0.05 |
+| `./node_modules/.bin/tsc dist/pickInterfaceState.ts` | 3.687 ± 0.048 | 3.644 | 3.866 | 1.00 |
